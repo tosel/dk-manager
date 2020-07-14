9f877a21e7177226498346ed04c4312d3fdf5ac5
Tobias Nagel <tobias@nagel-coe.de>
2020-07-14 14:46:23 +0200

    Init Logger

---
+++ src/de/villigst/dk/logic/Logger.java
@@ -0,0 +1,13 @@
+package de.villigst.dk.logic;
+
+public class Logger {
+
+    public static void info(String msg) {
+        System.out.println("[Info] " + msg);
+    }
+
+    public static void error(String msg) {
+        System.out.println("[ERROR] " + msg);
+    }
+
+}
