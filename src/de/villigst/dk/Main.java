61b0794762783114f30baaebc790b34ea53b57e7
Tobias Nagel <tobias@nagel-coe.de>
2020-07-14 15:16:46 +0200

    Implemented Namensschild conversion

--- src/de/villigst/dk/Main.java
+++ src/de/villigst/dk/Main.java
@@ -1,11 +1,15 @@
 package de.villigst.dk;
 
+import de.villigst.dk.logic.PDFConverter;
+import de.villigst.dk.view.UI;
+
 public class Main {
 
     public static void main(String[] args) {
         //Initialize UI
         new UI();
-
+        PDFConverter converter = new PDFConverter("C:\\Users\\topps\\OneDrive - ruhr-uni-bochum.de\\Uni\\Workspace\\DK Manager\\src\\de\\villigst\\dk\\template\\meldeschild_a4.html", "C:\\Users\\topps\\OneDrive - ruhr-uni-bochum.de\\Uni\\Workspace\\DK Manager\\out\\out.pdf");
+        converter.convert(true);
     }
 
 }
--- src/de/villigst/dk/logic/PDFConverter.java
+++ src/de/villigst/dk/logic/PDFConverter.java
@@ -1,6 +1,9 @@
 package de.villigst.dk.logic;
 
 import com.itextpdf.html2pdf.HtmlConverter;
+import com.itextpdf.kernel.geom.PageSize;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -16,21 +19,23 @@ public class PDFConverter {
         this.htmlSrc = htmlSrc;
         this.pdfDest = pdfDest;
 
-        File htmlSrcFile = new File(htmlSrc);
+        this.htmlSrcFile = new File(htmlSrc);
 
 
-        File pdfDestFile = new File(pdfDest);
+        this.pdfDestFile = new File(pdfDest);
         pdfDestFile.getParentFile().mkdirs();
 
     }
 
-    public File convert() {
+    public File convert(boolean landscape) {
         if(!htmlSrcFile.exists()) {
             Logger.error("Invalid source file: Not found");
             return null;
         }
         try {
-            HtmlConverter.convertToPdf(new FileInputStream(htmlSrcFile), new FileOutputStream(pdfDestFile));
+            PdfDocument pdfDocument = new PdfDocument(new PdfWriter(pdfDestFile));
+            if(landscape) pdfDocument.setDefaultPageSize(PageSize.A4.rotate());
+            HtmlConverter.convertToPdf(new FileInputStream(htmlSrcFile), pdfDocument);
             return pdfDestFile;
         }catch(IOException ex) {
             Logger.error("Conversion failed: " + ex.getMessage());
