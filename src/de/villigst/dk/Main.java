88b2539c7b8dcdccce12a7ffa70b36ecdc137d9f
Tobias Nagel <tobias@nagel-coe.de>
2020-07-14 14:20:10 +0200

    Basic UI

--- src/de/villigst/dk/Main.java
+++ src/de/villigst/dk/Main.java
@@ -1,5 +1,11 @@
 package de.villigst.dk;
 
 public class Main {
-    
+
+    public static void main(String[] args) {
+        //Initialize UI
+        new UI();
+
+    }
+
 }
---
+++ src/de/villigst/dk/UI.java
@@ -0,0 +1,17 @@
+package de.villigst.dk;
+
+import javax.swing.*;
+
+public class UI extends JFrame {
+
+    public UI() {
+        this.setTitle("DK Manager");
+        this.setSize(1000, 600);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+
+        
+        this.setVisible(true);
+    }
+
+}
