38f645afae0d16e7fb7c01c398679d05e749e172
Tobias Nagel <tobias@nagel-coe.de>
2020-07-16 01:01:13 +0200

    added random-lists and stats

--- src/de/villigst/dk/persistence/Persistent.java
+++ src/de/villigst/dk/persistence/Persistent.java
@@ -3,6 +3,7 @@ package de.villigst.dk.persistence;
 import de.villigst.dk.model.DKMember;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /***
@@ -15,5 +16,16 @@ public class Persistent {
 
     //Zu Speichern:
     public static List<DKMember> members = new ArrayList<>();
+
+    public static int stat_mg = 0;
+    public static int stat_wg = 0;
+    public static int stat_dg = 0;
+
+    public static int stat_ms = 0;
+    public static int stat_ws = 0;
+    public static int stat_ds = 0;
+
+    public static HashMap<String, List<DKMember>> random_lists = new HashMap<>();
+
 
 }
